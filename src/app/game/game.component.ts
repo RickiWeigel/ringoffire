3a8b9751c521427820803ba82e8f8a26ffb4545f
Ricki W. <ricki.weigel@gmx.de>
2023-12-29 20:26:38 +0100

    #

---
+++ src/app/game/game.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Route } from '@angular/router';
+
+@Component({
+  selector: 'app-game',
+  standalone: true,
+  imports: [],
+  templateUrl: './game.component.html',
+  styleUrl: './game.component.scss'
+})
+export class GameComponent {
+
+
+
+}
