4227115a90fc5edfa1b4afa9bbb969e8bfa74bcc
Ricki W. <ricki.weigel@gmx.de>
2024-01-03 16:13:57 +0100

    added animation

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,15 +1,27 @@
 import { Component } from '@angular/core';
-import { Route } from '@angular/router';
+import { NgForOf } from '@angular/common';
+import { NgStyle } from '@angular/common';
+import { NgIf } from '@angular/common';
+import { OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [],
+  imports: [NgForOf,NgStyle,NgIf],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
-export class GameComponent {
+export class GameComponent implements OnInit{
+pickCardAnimation = false;
 
+ngOnInit(): void {
+    
+}
 
+takeCard(){
+  this.pickCardAnimation = true;
+}
 
 }
+
